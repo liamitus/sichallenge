66f25ca4052995b25348a02693cefa07a50af2b8
Liam Howell <liammm@gmail.com>
2016-07-14 18:00:28 -0400

    Made create su deploy step more robust

--- reservations/management/commands/createsu.py
+++ reservations/management/commands/createsu.py
@@ -5,8 +5,8 @@ from django.contrib.auth.models import User
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        username = os.environ['ADMIN_USERNAME']
-        email = os.environ['ADMIN_EMAIL']
-        password = os.environ['ADMIN_PW']
-        if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser(username, email, password)
+        email = os.environ.get('ADMIN_EMAIL') if not None else 'admin@admin.com'
+        password = os.environ.get('ADMIN_PASSWORD') if not None else 'please_dont_hack'
+        
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser('admin', email, password)
